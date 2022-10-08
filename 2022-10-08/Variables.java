3e0965fc74b4f5714045c76357b8ef6435fc5e4f
ntm2022 <nguyenminhk2000@gmail.com>
2022-10-08 19:44:57 +0700

    create Variables

---
+++ 2022-10-08/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args){
+        /*Biến i kiểu số nguyên, có giá trị là 10*/
+        int i = 10;
+        System.out.println(i);
+        /*Biến f kiểu số thực, có giá trị là 20.5*/
+        float f = 20.5f;
+        System.out.println(f);
+        /*Biến d kiểu số thực lớn, có giá trị là 20.5*/
+        double d = 20.5;
+        System.out.println(d);
+        /*Biến b kiểu logic, có giá trị là true*/
+        boolean b = true;
+        System.out.println(b);
+       /* Biến c kiểu ký tự, có giá trị là 'a'*/
+        char c = 'a';
+        System.out.println(c);
+        /*Biến s kiểu chuỗi, có giá trị là "Hà Nội".*/
+        String s = "Hà Nội";
+        System.out.println(s);
+    }
+}

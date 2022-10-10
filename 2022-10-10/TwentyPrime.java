2a43e13f65499e16c1437ba7f4e7e7e9eb386f74
ntm2022 <nguyenminhk2000@gmail.com>
2022-10-10 10:10:22 +0700

    create TwentyPrime

---
+++ 2022-10-10/TwentyPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class TwentyPrime {
+    public static void main(String[] args) {
+        int numbers;//numbers này sẽ in ra bao nhiêu số nguyên tố
+        int count = 0;
+        int N = 2;//dãy số nguyên tố tính từ 2
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số nguyên tố đầu tiên cần in ra: ");
+        numbers = scanner.nextInt();
+
+        while (count < numbers){
+                //kiểm tra nếu là số nguyên tố thì in ra
+                if (isPrime(N)) {
+                    System.out.println(N);
+                    count++;//neu laf so nguyen to thi bat dau dem
+                }
+            N++;
+        }
+    }
+    //count 0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19
+    //N tang mai
+    private static boolean isPrime(int count){ //thêm static ở đây để dùng thẳng hàm này mà k cần tạo đối tượng
+        for(int i=2;i<count;i++){
+            if(count % i==0) return false;
+        }
+        return true;
+    }
+}

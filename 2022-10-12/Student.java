8f23c26bb24fe0fb3911785f6380588cca147d32
ntm2022 <nguyenminhk2000@gmail.com>
2022-10-12 16:00:44 +0700

    create class student

---
+++ 2022-10-12/Student.java
@@ -0,0 +1,23 @@
+package com.codegym;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    //constructor to initialize the variable
+    Student(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    //static method to change the value of static variable
+    static void change() {//phuong thuc nay chi chay 01 lan du new bao nhieu lan di nua???
+        college = "CODEGYM";
+    }
+
+    //method to display values
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}

88b4f789c5b94cc5ca91d3a782545acd44070c2e
ntm2022 <nguyenminhk2000@gmail.com>
2022-10-16 20:34:41 +0700

    create ComparableCircle

---
+++ 2022-10-16/ComparableCircle/ComparableCircle.java
@@ -0,0 +1,28 @@
+package com.codegym;
+
+public class ComparableCircle extends Circle
+        implements Comparable<ComparableCircle> {
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {//thiếu tham số bên hàm khởi tạo lớp Circle???
+        super(radius, color, filled);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+
+    @Override
+    public String toString() {//thiếu hàm toString bên lớp Circle
+        return super.toString();
+    }
+}

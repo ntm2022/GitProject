f3d12477160823cacc5e66bee7705919e97c3850
ntm2022 <112862523+ntm2022@users.noreply.github.com>
2022-10-24 11:17:51 +0700

    Delete BubbleSortMain.java

--- BubbleSortMain.java
+++
@@ -1,7 +0,0 @@
-public class BubbleSortMain {
-    public static void main(String[] args) {
-        BubbleSort.bubbleSort(BubbleSort.list);
-        for (int i = 0; i < BubbleSort.list.length; i++)
-            System.out.print(BubbleSort.list[i] + " ");
-    }
-}

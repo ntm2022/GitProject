afa50838034a04fe710287dbbf3e4b7c013f04c4
ntm2022 <nguyenminhk2000@gmail.com>
2022-10-24 11:51:55 +0700

    create BinarySearch

---
+++ TIMKIEM/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* vị trí 0 */
+        System.out.println(binarySearch(list, 11)); /* vị trí 4 */
+        System.out.println(binarySearch(list, 79)); /*vị trí 12 */
+        System.out.println(binarySearch(list, 1));  /*-1 k tìm thấy*/
+        System.out.println(binarySearch(list, 5));  /*-1 k tìm thấy*/
+        System.out.println(binarySearch(list, 80)); /*-1 k tìm thấy*/
+    }
+    static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+        while (high >= low) {
+            int mid = (low + high) / 2;
+            if (key < list[mid])
+                high = mid - 1;
+            else if (key == list[mid])
+                return mid;
+            else
+                low = mid + 1;
+        }
+        return -1; /* Now high < low, key not found */
+    }
+
+
+}
